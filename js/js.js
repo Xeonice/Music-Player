40a2ad5841666b4f22fa510756d5b706b5f91e90
唐和辉 <yourgitemail@example.com>
2017-07-13 03:07:46 +0800

    添加css动画，准备处理css动画与播放按钮同步的问题

--- js/js.js
+++ js/js.js
@@ -1,12 +1,15 @@
+var d=document.getElementsByClassName('Rotation');
 function playPause() {
     var music = document.getElementById('music2');
     var music_btn = document.getElementById('music_btn2');
     if (music.paused){
         music.play();
         music_btn.src = 'image/pause.png';
+        d.style.animationPlayState="running";
     }
     else{
         music.pause();
         music_btn.src = 'image/play.png';
+        d.style.animationPlayState="paused";
     }
 }
