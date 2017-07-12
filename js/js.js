b169461456080f281da020c05e578b8ac0832938
唐和辉 <yourgitemail@example.com>
2017-07-13 03:52:18 +0800

    成功添加动画效果

--- js/js.js
+++ js/js.js
@@ -1,15 +1,23 @@
-var d=document.getElementsByClassName('Rotation');
 function playPause() {
     var music = document.getElementById('music2');
     var music_btn = document.getElementById('music_btn2');
     if (music.paused){
         music.play();
         music_btn.src = 'image/pause.png';
-        d.style.animationPlayState="running";
     }
     else{
         music.pause();
         music_btn.src = 'image/play.png';
-        d.style.animationPlayState="paused";
     }
+}
+
+window.onload = function () {
+    var btn=document.getElementById('music_btn2');
+    btn.addEventListener('click', setAnimation, false);
+};
+function setAnimation() {
+    var playstate;
+    var am=document.getElementById('Rotation');
+    am.style.WebkitAnimationPlayState == 'running' ? playstate='paused': playstate= 'running';
+    am.style.WebkitAnimationPlayState = playstate;
 }
